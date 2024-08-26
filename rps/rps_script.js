3bc793a5d888e59584efc3dea479f17542484014
Vishnu <92211782+vishnu-sk@users.noreply.github.com>
2024-08-26 23:49:39 +0530

    added final scoreboard in html

--- rps/rps_script.js
+++ rps/rps_script.js
@@ -1,22 +1,18 @@
 //  let playerChoice = getPlayerChoice();
 //  let computerChoice = getComputerChoice();
-let playerScore = 0; 
+let playerScore = 0;
 let computerScore = 0;
 
-function getPlayerChoice()
-{
+function getPlayerChoice() {
     return prompt("What do you want to choose? Rock, Paper Scissors");
 }
-function getComputerChoice()
-{
+function getComputerChoice() {
     let randInt = Math.random();
-    if(randInt<=0.33)
-    {
+    if (randInt <= 0.33) {
         console.log("comp choice: rock " + randInt);
         return "rock";
     }
-    else if(randInt >= 0.66)
-    {
+    else if (randInt >= 0.66) {
         console.log("comp choice: scissors " + randInt);
         return "scissors";
     }
@@ -25,57 +21,51 @@ function getComputerChoice()
         return "paper";
     }
 }
-function selectWinner(player, comp)
-{
+function selectWinner(player, comp) {
     player = player.toLowerCase();
-    if(player == "rock")
-    {
-        if(comp == "paper") return -1;
-        else if(comp == "scissors") return 1;
+    if (player == "rock") {
+        if (comp == "paper") return -1;
+        else if (comp == "scissors") return 1;
         else return 0;
     }
-    else if(player == "paper")
-    {
-        if(comp == "paper") return 0;
-        else if(comp == "scissors") return -1;
+    else if (player == "paper") {
+        if (comp == "paper") return 0;
+        else if (comp == "scissors") return -1;
         else return 1;
     }
-    else
-    {
-        if(comp == "paper") return 1;
-        else if(comp == "scissors") return 0;
+    else {
+        if (comp == "paper") return 1;
+        else if (comp == "scissors") return 0;
         else return -1;
     }
 }
-function playRound()
-{
-    let outcome =selectWinner(getPlayerChoice(), getComputerChoice());
-    if(outcome == 1)
-    {
+function playRound() {
+    let outcome = selectWinner(getPlayerChoice(), getComputerChoice());
+    if (outcome == 1) {
         playerScore++;
     }
-    else if(outcome == -1)
-    {
+    else if (outcome == -1) {
         computerScore++;
     }
+    document.getElementById("playerscore").innerHTML = playerScore;
+    document.getElementById("compscore").innerHTML = computerScore;
 }
-function playGame(){
-    for( i = 0; i < 5; i++)
-    {
+function playGame() {
+    playerScore = 0; computerScore = 0;
+    for (i = 0; i < 2; i++) {
         playRound();
+
+        console.log(playerScore + " x " + computerScore);
     }
-    console.log("Player score: " + playerScore );
+    console.log("Player score: " + playerScore);
     console.log("Computer score: " + computerScore);
-    if(playerScore > computerScore)
-    {
+    if (playerScore > computerScore) {
         console.log("Player Won!!");
     }
-    else if(playerScore<computerScore)
-    {
+    else if (playerScore < computerScore) {
         console.log("Computer Won!!");
     }
-    else
-    {
+    else {
         console.log("Game is a draw");
     }
 }
