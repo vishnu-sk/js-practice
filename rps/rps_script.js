502b13ad3915083637af874b8f8769b1efa109d7
Vishnu <92211782+vishnu-sk@users.noreply.github.com>
2024-09-19 19:42:29 +0530

    implemented choices and results element and reset action in DOM

--- rps/rps_script.js
+++ rps/rps_script.js
@@ -6,11 +6,16 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const c = document.querySelector(".button-class");
-c.addEventListener("click",(e)=>{playRound(e.target.id);})
-// rock.addEventListener("click", getPlayerChoice);
-// function getPlayerChoice(e) {
-//     playRound(e);
-// }
+const playBtn = document.getElementsByClassName("play")[0];
+
+c.addEventListener("click",(e)=>{
+    if(e.target.id === "rock"){playRound("rock", getComputerChoice());}
+    else if(e.target.id === "paper"){playRound("paper",getComputerChoice());}
+    else if(e.target.id==="scissors"){playRound("scissors",getComputerChoice());}
+});
+
+playBtn.addEventListener("click",reset);
+
 function getComputerChoice() {
     let randInt = Math.random();
     if (randInt <= 0.33) {
@@ -26,6 +31,7 @@ function getComputerChoice() {
         return "paper";
     }
 }
+
 function selectWinner(player, comp) {
     player = player.toLowerCase();
     if (player == "rock") {
@@ -44,30 +50,52 @@ function selectWinner(player, comp) {
         else return -1;
     }
 }
-function playRound(playerChoice) {
-    // let outcome = selectWinner(getPlayerChoice(), getComputerChoice());
-    let outcome = selectWinner(playerChoice, getComputerChoice());
+
+function playRound(playerChoice, computerChoice) {
+    let outcome = selectWinner(playerChoice, computerChoice);
     if (outcome == 1) {
         playerScore++;
     }
     else if (outcome == -1) {
         computerScore++;
     }
-    document.getElementById("playerscore").innerText = playerScore;
-    document.getElementById("compscore").innerText = computerScore;
+    updateChoiceUI(playerChoice,computerChoice);
+    updateScoreUI(playerScore,computerScore)
+    if(playerScore >= 5 || computerScore >= 5){
+        playGame();
+    }
 }
-function playGame() {
-    playerScore = 0; computerScore = 0;
-    
+
+function updateChoiceUI(playerChoice, computerChoice){
+    document.getElementsByClassName("your-choice")[0].innerHTML = playerChoice;
+    document.getElementsByClassName("comp-choice")[0].innerText = computerChoice
+}
+
+function updateScoreUI(playerScore, computerScore){
+    document.getElementById("playerscore").innerText = playerScore;
+    document.getElementById("compscore").innerText = computerScore
+}
+
+function playGame() {    
     console.log("Player score: " + playerScore);
     console.log("Computer score: " + computerScore);
+    const results =  document.querySelector("h2");
     if (playerScore > computerScore) {
         console.log("Player Won!!");
+        results.innerHTML = "Player Won!!";
     }
     else if (playerScore < computerScore) {
         console.log("Computer Won!!");
+        results.innerHTML = "Computer Won!!";
     }
     else {
         console.log("Game is a draw");
     }
+}
+
+function reset()
+{
+    playerScore = 0;
+    computerScore = 0;
+    updateScoreUI(playerScore,computerScore);
 }
