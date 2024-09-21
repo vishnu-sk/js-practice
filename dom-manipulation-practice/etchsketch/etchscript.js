602e7dca445f977d0d6520197ffb3c85802df66b
Vishnu <92211782+vishnu-sk@users.noreply.github.com>
2024-09-22 02:53:52 +0530

    Added 16x16 grid with flexbox with color change on hover feature

--- dom-manipulation-practice/etchsketch/etchscript.js
+++ dom-manipulation-practice/etchsketch/etchscript.js
@@ -1,6 +1,26 @@
 const cont = document.querySelector(".grid-container");
-for(let i=0; i< 16; i++){
-    let elem = document.createElement("div");
-    cont.appendChild(elem);
+for (let i = 0; i < 16; i++) {
+  let elem = document.createElement("div");
+  cont.appendChild(elem);
+  elem.setAttribute("class", "div-block");
 }
-document.getElementsByClassName("grid-container")[0].style.display = "flex";
+divelem = document.getElementsByClassName("div-block");
+cont.style.display = "flex";
+cont.style.flexWrap = "wrap";
+
+const styleDiv = {
+  height: "25%",
+  width: "25%",
+};
+for (let el of divelem) {
+  Object.assign(el.style, styleDiv);
+  el.addEventListener("mouseenter",()=>{
+    el.style.backgroundColor = "blue";
+  });
+}
+
+//adding button above container
+const elem = document.createElement("button");
+elem.innerText = "New";
+const body = document.querySelector("body");
+body.insertBefore(elem, cont);
